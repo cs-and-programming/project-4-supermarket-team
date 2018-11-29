02309ddfa180b64ec792e8024d3e2110320d0627
Dave Peacock <peacoc24@students.rowan.edu>
2018-11-28 19:55:19 -0500

    new changes

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,31 +1,45 @@
-#include "counter.h"
+#include "product.h"
 #include <iostream>
 
 using namespace std;
 
+product Eggs = 
+product Milk = 
+product Bread = 
+product Soda = 
+product Meat = 
+product Cereal = 
+
+
+
 int main()
 {
-	std::cout << "Hello World!\n";
-	counter c1;
-	counter c2(3);
-	counter c3(10);
 
-	c1.increment();
-	cout << "this is the counter value" << c1.getCount() << endl;
+	cout << "Welcome to the Sweep Mart's stock program\n";
+	cout << "Select the item you would like to display information about:" << endl;
 
-	for (int i = 0; i < 5; i++)
-	{
-		c1.increment();
-		cout << "this is the counter value" << c1.getCount() << endl;
-	}
 
-	for (int i = 0; i < 4; i++)
-	{
-		c2.increment();
-		cout << "this is the counter value " << c2.getCount() << endl;
-	}
+	
+	//counter c1;
+	//counter c2(3);
+	//counter c3(10);
 
-	c3.setCount(501);
+	//c1.increment();
+	//cout << "this is the counter value" << c1.getCount() << endl;
+
+	//for (int i = 0; i < 5; i++)
+	//{
+		//c1.increment();
+		//cout << "this is the counter value" << c1.getCount() << endl;
+	//}
+
+	//for (int i = 0; i < 4; i++)
+	//{
+		//c2.increment();
+		//cout << "this is the counter value " << c2.getCount() << endl;
+	//}
+
+	//c3.setCount(501);
 
 	system("pause");
 
